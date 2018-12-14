3384ccf0464f28fc02da9c976ea1d94247ee8fb6
avil13 <13.avil@gmail.com>
2018-12-15 01:17:57 +0300

    rc

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const btn = document.getElementById('btn');
 
+var actionsObject = {
+    commands: [ 'открыть забрало', 'закрыть забрало', 'открыть глаза', 'закрыть глаза' ],
+    actions: {
+        '0': openMask,
+        '1': closeMask,
+        '2': openEyes,
+        '3': closeEyes
+    }
+};
+
 if ('webkitSpeechRecognition' in window) {
     const recognition = new webkitSpeechRecognition();
 
@@ -8,11 +18,13 @@ if ('webkitSpeechRecognition' in window) {
             const result = event.results[i];
             const resultsAlternative = result[0];
             console.log(resultsAlternative.transcript);
+            findCommand.call(actionsObject, resultsAlternative.transcript);
         }
     };
 
     recognition.lang = 'ru-RU';
-    
+    addGrammar.call(actionsObject, recognition);
+
     btn.addEventListener('click', (ev) => {
         ev.target.classList.add('active');
 
@@ -27,3 +39,77 @@ if ('webkitSpeechRecognition' in window) {
     console.warning('No speech');
 }
 
+function addGrammar(recognition) {
+    const grammar = `
+    #JSGF v1.0;
+    grammar actions;
+    public <actions> = ${this.commands.join(' | ')} ;
+    `;
+    var speechRecognitionList = new webkitSpeechGrammarList();
+    speechRecognitionList.addFromString(grammar, 1);
+    recognition.grammars = speechRecognitionList;
+    recognition.interimResults = false;
+    recognition.continuous = true;
+    recognition.maxAlternatives = 1;
+    // recognition.lang = 'en-US';
+    recognition.grammars = speechRecognitionList;
+}
+
+function findCommand(text) {
+    const command = this.commands.find((v) => {
+        return text.indexOf(v) > -1;
+    });
+
+    if (command) {
+        const action = this.actions[this.commands.indexOf(command)];
+        action();
+    } else {
+        tell('Я тебя не понимаю, попробуй еще раз');
+    }
+}
+
+function tell(text) {
+    const synthesis = window.speechSynthesis;
+    const textToSpeech = new SpeechSynthesisUtterance(text);
+    synthesis.speak(textToSpeech);
+}
+
+function openMask() {
+    tell('Давай представим, как будто маска открывается');
+    // $select([ '.head-left', '.head-right' ], (el) => {
+    //     el.
+    // });
+}
+
+function closeMask() {
+    tell('Давай представим, как будто маска закрывается');
+}
+
+function openEyes() {
+    tell('Открываю глаза');
+    $select('.container', (el) => {
+        el.classList.add('open-eyes');
+    });
+}
+
+function closeEyes() {
+    tell('Закрываю глаза');
+    $select('.container', (el) => {
+        el.classList.remove('open-eyes');
+    });
+}
+
+function $select(selectors, callback) {
+    if (!Array.isArray(selectors)) {
+        selectors = [ selectors ];
+    }
+    selectors
+        .map((cl) => document.querySelector(cl)) ///
+        .map((el) => {
+            if (el) {
+                callback(el);
+            } else {
+                console.error('Не нашли элемента');
+            }
+        });
+}
